debbc3964472d70406e292be7a16f0eff668d999
PaigeDoucette57 <paige.doucette@maine.edu>
2026-02-02 14:58:08 -0500

    Completed HTML, CSS, Button, and red boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./App.css";
+import MyImage from './assets/images/image.jpg';
+import { Button } from 'react-bootstrap';
+
+
+
+
 
 function App(): React.JSX.Element {
     return (
@@ -7,13 +13,47 @@ function App(): React.JSX.Element {
             <header className="App-header">
                Paige Doucette- UM COS420 with React Hooks and TypeScript 
             </header>
+
+
+
+            {/*Button */}
+            <Button onClick={() => console.log("Hello World!")}>
+          Log Hello World
+
+            </Button>
+
+
+
+            {/* Red boxes*/}
+            <div style={{ width: '100px', height: '100px', backgroundColor: 'red' }}></div>
+            <div style={{ width: '100px', height: '100px', backgroundColor: 'red' }}></div>
+
+            {/* Heading */}
+            <h1>Hello World</h1>
+
+            {/* Image */}
+            <img src={MyImage} alt="Photo of me and friend diving" />
+
+
+
+            {/* Paragraph*/}
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Paige Doucette 
+            Edit <code>src/App.tsx</code> and save. This page will
+            automatically reload. Paige Doucette 
                 Hello World 
             </p>
+
+
+            {/*List*/}
+            <ul>
+                <li>Apple </li>
+                <li>Pear </li>
+                <li>Orange </li>
+            </ul>
         </div>
     );
 }
 
 export default App;
+
+
